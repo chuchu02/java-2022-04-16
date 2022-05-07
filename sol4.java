4b036474eb9977c83401bedc1d95c6bacace281e
chuchu02 <103809166+chuchu02@users.noreply.github.com>
2022-05-07 14:40:51 +0900

    Add files via upload

---
+++ sol4.java
@@ -0,0 +1,41 @@
+package java_exam;
+
+public class sol4 {
+
+	public static void main(String[] args) {
+		String[] students = {"Ã¶¼ö", "¿µÈñ", "¹ÎÃ¶", "Áø¼ö", "ÁøÈñ"};
+		int[] scores = {100, 90, 80, 70, 60};
+		int[] heights = {180, 163, 177, 168, 153};
+		
+		Student s1 = new Student("Ã¶¼ö", 100, 180);
+
+		Student s1 = new Student("Ã¶¼ö", 100, 180);
+
+		Student s1 = new Student("Ã¶¼ö", 100, 180);
+
+		Student s1 = new Student("Ã¶¼ö", 100, 180);
+
+		Student s1 = new Student("Ã¶¼ö", 100, 180);
+		int sum = 0;
+		for(int i =0; i < students.length; i++) {
+			Student s = students[i];
+			sum += s.score;
+		}
+
+	}
+
+}
+
+
+class Student{
+	String name;
+	int score;
+	int height;
+	
+	public student{int data1, String data2, int data3){
+		score = data1;
+		name = data2;
+		height = data3;
+	}
+	public in
+}
