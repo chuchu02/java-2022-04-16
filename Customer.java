92aa43c00e35516831f13539124d6ad86728f134
chuchu02 <103809166+chuchu02@users.noreply.github.com>
2022-05-08 12:47:40 +0900

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -5,7 +5,7 @@ public class Customer {
 	String name;
 	String address;
 	
-	public Customer(int data1, String data2, String data3) {
+	public Customer(int data1, String data2, String data3) {//class와 같은 이름을 쓰고 매개변수를 공유
 		age = data1;
 		name = data2;
 		address = data3;
